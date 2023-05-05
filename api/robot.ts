474aa274ceff05557b5a3c50043670d32b51926c
gengyi <609924321@qq.com>
2023-05-05 13:47:50 +0800

    api change commit

---
+++ api/robot.ts
@@ -0,0 +1,50 @@
+import request from '../utils/request';
+
+/**
+ * 工坊列表
+ */
+export const getUgcBotList = (params) => {
+  return request({
+    url: `/bot/getUgcBotList`,
+    method: 'get',
+    params
+  })
+}
+
+/**
+ * 工坊列表
+ */
+export const queryCanCreateUgcBot = (params) => {
+  return request({
+    url: `/bot/canCreateUgcBot`,
+    method: 'get',
+    params
+  })
+}
+
+/**
+ * 工坊详情
+ */
+export const getUgcBotDetail = (params) => {
+  return request({
+    url: `/bot/getUgcBotDetail`,
+    method: 'get',
+    params
+  })
+}
+
+export const postAddBotToChatList = data => {
+  return request({
+    url: `/bot/addBotToChatList`,
+    method: 'post',
+    data,
+  })
+}
+
+export const postPublishBot = data => {
+  return request({
+    url: `/bot/publishBot`,
+    method: 'post',
+    data,
+  })
+}
