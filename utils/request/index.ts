a65b8e3f973791550b2381f2b679055b0c076d0e
winljm001 <819648983@qq.com>
2023-05-09 23:21:07 +0800

    feat: 解决引用循环警告

--- utils/request/index.ts
+++ utils/request/index.ts
@@ -1,88 +1,91 @@
-import { Toast } from '@fruits-chain/react-native-xiaoshu'
-import type { AxiosRequestConfig, AxiosResponse } from 'axios'
-import axios from 'axios'
-import useUserStore from '../../store/userStore'
+import { Toast } from "@fruits-chain/react-native-xiaoshu";
+import type { AxiosRequestConfig, AxiosResponse } from "axios";
+import axios from "axios";
+import useUserStore from "../../store/userStore";
 
-import systemConfig from '../../constants/System'
-import MSG_LIST from './message'
-import debounce from 'lodash/debounce'
+import systemConfig from "../../constants/System";
+import MSG_LIST from "./message";
+import debounce from "lodash/debounce";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 export type RequestOptions = AxiosRequestConfig & {
-  url: string
+  url: string;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  body?: any
+  body?: any;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  headers?: any
-}
+  headers?: any;
+};
 
 const toastError = (msg: string) => {
   Toast({
     message: msg,
     duration: 1500,
-  })
-}
-const errorTip = debounce(toastError, 500)
+  });
+};
+const errorTip = debounce(toastError, 500);
 
-const { baseUrl, authKey } = systemConfig
-const _axios = axios.create()
+const { baseUrl, authKey } = systemConfig;
+const _axios = axios.create();
 /**
  * 响应拦截器
  */
 _axios.interceptors.response.use(
   (response: AxiosResponse) => {
     // TODO fix it
-    const result: { errCode: number; errMsg: string; data: unknown } = response.data
+    const result: { errCode: number; errMsg: string; data: unknown } =
+      response.data;
     // 图片上传简易判断
     if (!result.errCode) {
-      return response
+      return response;
     }
     // 错误并提示
     if (result.errCode !== 200) {
-      const errText = `${result.errMsg}`
+      const errText = `${result.errMsg}`;
       Toast({
         message: errText,
         duration: 2500,
-      })
-      return Promise.reject(errText)
+      });
+      return Promise.reject(errText);
     }
-    return response
+    return response;
   },
-  error => {
-    console.log(error, 'error')
-    const { response } = error
+  (error) => {
+    console.log(error, "error");
+    const { response } = error;
     // 请求有响应
     if (response) {
-      const { status, data, config } = response
+      const { status, data, config } = response;
       if (status === 401) {
         // 状态码为401时，根据白名单来判断跳转与否
-        errorTip(data.message || '')
-        return Promise.reject(new Error(data.message))
+        errorTip(data.message || "");
+        return Promise.reject(new Error(data.message));
       }
       // 404 502 ..
-      errorTip(data.message || '')
-      return Promise.reject(data.message)
+      errorTip(data.message || "");
+      return Promise.reject(data.message);
       // throw message;
     }
     // 请求超时
-    if (error.code === 'ECONNABORTED') {
-      const timeoutMsg = MSG_LIST.timeout
-      return Promise.reject(timeoutMsg)
+    if (error.code === "ECONNABORTED") {
+      const timeoutMsg = MSG_LIST.timeout;
+      return Promise.reject(timeoutMsg);
     }
-    return Promise.reject(MSG_LIST.network)
+    return Promise.reject(MSG_LIST.network);
   }
-)
+);
 // TODO: 添加options 类型interface
 export default async function request<T>(options: RequestOptions) {
-  const { url } = options
-  const opt: RequestOptions = options
-  delete opt.url
-  const token = useUserStore.getState().userBaseInfo?.token
-  if (!token && url !== '/auth/particleLogin') {
-    return
+  const { url } = options;
+  const opt: RequestOptions = options;
+  delete opt.url;
+  // const token = useUserStore.getState().userBaseInfo?.token
+  const token = await AsyncStorage.getItem(authKey);
+  if (!token && url !== "/auth/particleLogin") {
+    return;
   }
-  const Authorization = token ? `Bearer ${token}` : ''
-  let headers = {}
+  const Authorization = token ? `Bearer ${token}` : "";
+  let headers = {};
   if (options) {
-    headers = options.headers || {}
+    headers = options.headers || {};
   }
   const defaultOptions = {
     headers: {
@@ -90,22 +93,22 @@ export default async function request<T>(options: RequestOptions) {
       appversioncode: 3,
       ...headers,
     },
-    credentials: 'include',
+    credentials: "include",
     timeout: 10000,
     withCredentials: true,
     validateStatus(status: number) {
-      return status >= 200 && status < 300 // default
+      return status >= 200 && status < 300; // default
     },
-  }
+  };
   if (options) {
-    delete options.headers
+    delete options.headers;
   }
-  const newOptions: RequestOptions = { ...defaultOptions, ...options }
-  const newUrl = baseUrl + url
+  const newOptions: RequestOptions = { ...defaultOptions, ...options };
+  const newUrl = baseUrl + url;
   return _axios
     .request<T>({
       ...newOptions,
       url: newUrl,
     })
-    .then(data => data.data)
+    .then((data) => data.data);
 }
