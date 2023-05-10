630d602d419db7c258635537b464b368d235e979
weishunyu <2072738187@qq.com>
2023-05-11 00:58:42 +0800

    fix： 高度变化弹簧

--- components/profileInfo/EditAvatarModal.tsx
+++ components/profileInfo/EditAvatarModal.tsx
@@ -1,63 +1,58 @@
-import { getProfile, UserProfile } from "../../store/userStore";
-import { Button, Overlay } from "@fruits-chain/react-native-xiaoshu";
-import { Toast } from "../../utils/toast";
-import { FC, useEffect, useRef, useState } from "react";
-import * as ImagePicker from "expo-image-picker";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import { Image } from "expo-image";
-import ArrowLeft from "../../assets/images/profile/arrow-left.svg";
-import request from "../../utils/request";
+import { getProfile, UserProfile } from '../../store/userStore'
+import { Button, Overlay } from '@fruits-chain/react-native-xiaoshu'
+import { Toast } from '../../utils/toast'
+import { FC, useEffect, useRef, useState } from 'react'
+import * as ImagePicker from 'expo-image-picker'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { Image } from 'expo-image'
+import ArrowLeft from '../../assets/images/profile/arrow-left.svg'
+import request from '../../utils/request'
 
-import ImgPlaceholder from "../../assets/images/img_placeholder.png";
-import CustomSlider from "./Slider";
-import { genAvatarUrl } from "./helper";
-import ViewShot, { captureRef } from "react-native-view-shot";
-import { useBoolean } from "ahooks";
-const uploadFile = async (uri) => {
-  const filePath = uri;
-  const formData = new FormData();
-  formData.append("file", {
+import ImgPlaceholder from '../../assets/images/img_placeholder.png'
+import CustomSlider from './Slider'
+import { genAvatarUrl } from './helper'
+import ViewShot, { captureRef } from 'react-native-view-shot'
+import { useBoolean } from 'ahooks'
+const uploadFile = async uri => {
+  const filePath = uri
+  const formData = new FormData()
+  formData.append('file', {
     uri: filePath,
-    type: "application/octet-stream",
-    name: "file",
-  });
+    type: 'application/octet-stream',
+    name: 'file',
+  } as any)
 
-  const boundary = "----WebKitFormBoundaryKuaSUT6xPZeD4B8b";
+  const boundary = '----WebKitFormBoundaryKuaSUT6xPZeD4B8b'
   return await request({
-    url: "/user/uploadAvatar",
-    method: "post",
+    url: '/user/uploadAvatar',
+    method: 'post',
     data: formData,
     headers: {
-      "Content-Type": `multipart/form-data; boundary=${boundary}`,
+      'Content-Type': `multipart/form-data; boundary=${boundary}`,
     },
-  });
-};
+  })
+}
 
 export interface EditAvatarModalProps {
-  visible: boolean;
-  setVisible: (val: boolean) => void;
-  profile: UserProfile;
+  visible: boolean
+  setVisible: (val: boolean) => void
+  profile: UserProfile
 }
-const EditAvatarModal: FC<EditAvatarModalProps> = ({
-  visible,
-  setVisible,
-  profile,
-}) => {
-  const viewRef = useRef();
-  const [imgSize, setImgSize] = useState(0);
-  const [zoom, setZoom] = useState(1);
-  const [zoomSize, setZoomSize] = useState(0);
-  const [updateLoading, { set: setUpdateLoading }] = useBoolean(false);
+const EditAvatarModal: FC<EditAvatarModalProps> = ({ visible, setVisible, profile }) => {
+  const viewRef = useRef()
+  const [imgSize, setImgSize] = useState(0)
+  const [zoom, setZoom] = useState(1)
+  const [zoomSize, setZoomSize] = useState(0)
+  const [updateLoading, { set: setUpdateLoading }] = useBoolean(false)
   useEffect(() => {
-    setZoomSize(imgSize);
-  }, [imgSize]);
+    setZoomSize(imgSize)
+  }, [imgSize])
   useEffect(() => {
     if (visible) {
-      setInputImage(null);
+      setInputImage(null)
     }
-  }, [visible]);
-  const [inputImage, setInputImage] =
-    useState<ImagePicker.ImagePickerAsset>(null);
+  }, [visible])
+  const [inputImage, setInputImage] = useState<ImagePicker.ImagePickerAsset>(null)
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -65,57 +60,54 @@ const EditAvatarModal: FC<EditAvatarModalProps> = ({
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
-    });
+    })
 
     if (!result.canceled) {
-      setInputImage(result.assets[0]);
+      setInputImage(result.assets[0])
     }
-  };
+  }
 
   const updateAvatar = async () => {
-    setUpdateLoading(true);
+    setUpdateLoading(true)
     try {
       captureRef(viewRef, {
-        format: "png",
+        format: 'png',
         quality: 1,
       }).then(
-        (uri) => {
+        uri => {
           uploadFile(uri)
             .then(() => {
-              Toast("Update successfully!");
-              setVisible(false);
-              getProfile();
+              Toast('Update successfully!')
+              setVisible(false)
+              getProfile()
             })
             .finally(() => {
-              setUpdateLoading(false);
-            });
+              setUpdateLoading(false)
+            })
         },
-        (error) => {
-          console.log(error);
-          setUpdateLoading(false);
+        error => {
+          console.log(error)
+          setUpdateLoading(false)
         }
-      );
+      )
       // if (inputImage?.fileSize > 1048576) {
       //   Toast("The image size cannot exceed 1M");
       //   return false;
       // }
     } catch (error) {
-      console.log({ error });
+      console.log({ error })
     }
-  };
+  }
 
   return (
-    <Overlay
-      visible={visible}
-      style={{ justifyContent: "center", alignItems: "center" }}
-    >
+    <Overlay visible={visible} style={{ justifyContent: 'center', alignItems: 'center' }}>
       <View style={styles.cardWrap}>
         <View style={styles.card}>
           <View style={styles.header}>
             <View style={styles.headerLeft}>
               <TouchableOpacity
                 onPress={() => {
-                  setVisible(false);
+                  setVisible(false)
                 }}
               >
                 <ArrowLeft />
@@ -128,7 +120,7 @@ const EditAvatarModal: FC<EditAvatarModalProps> = ({
               size="s"
               style={{ borderRadius: 12, paddingHorizontal: 14 }}
               onPress={() => {
-                updateAvatar();
+                updateAvatar()
               }}
             >
               Confirm
@@ -136,14 +128,14 @@ const EditAvatarModal: FC<EditAvatarModalProps> = ({
           </View>
           <View
             style={styles.content}
-            onLayout={(e) => {
+            onLayout={e => {
               // 需要减边框的8像素
-              setImgSize(e?.nativeEvent?.layout?.width - 8);
+              setImgSize(e?.nativeEvent?.layout?.width - 8)
             }}
           >
             <TouchableOpacity
               onPress={() => {
-                pickImage();
+                pickImage()
               }}
             >
               <View
@@ -151,14 +143,14 @@ const EditAvatarModal: FC<EditAvatarModalProps> = ({
                   width: imgSize,
                   height: imgSize,
                   borderWidth: 4,
-                  borderColor: "#7A2EF6",
-                  overflow: "hidden",
+                  borderColor: '#7A2EF6',
+                  overflow: 'hidden',
                 }}
               >
                 <View
                   style={{
-                    width: "100%",
-                    height: "100%",
+                    width: '100%',
+                    height: '100%',
                   }}
                   ref={viewRef}
                 >
@@ -167,9 +159,7 @@ const EditAvatarModal: FC<EditAvatarModalProps> = ({
                     placeholderContentFit="cover"
                     placeholder={ImgPlaceholder}
                     source={{
-                      uri: genAvatarUrl(
-                        inputImage?.uri ? inputImage?.uri : profile?.avatar
-                      ),
+                      uri: genAvatarUrl(inputImage?.uri ? inputImage?.uri : profile?.avatar),
                     }}
                     style={{
                       width: zoomSize,
@@ -179,59 +169,52 @@ const EditAvatarModal: FC<EditAvatarModalProps> = ({
                   />
                   <Image
                     source={{
-                      uri: genAvatarUrl(
-                        inputImage?.uri ? inputImage?.uri : profile?.avatar
-                      ),
+                      uri: genAvatarUrl(inputImage?.uri ? inputImage?.uri : profile?.avatar),
                     }}
                   />
                 </View>
               </View>
             </TouchableOpacity>
-            <CustomSlider
-              value={zoom}
-              onValueChange={setZoom}
-              maximumValue={2}
-              minimumValue={1}
-            />
+            <CustomSlider value={zoom} onValueChange={setZoom} maximumValue={2} minimumValue={1} />
           </View>
         </View>
       </View>
     </Overlay>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   cardWrap: {
     paddingHorizontal: 24,
-    width: "100%",
-    alignItems: "center",
+    width: '100%',
+    alignItems: 'center',
   },
   card: {
     maxWidth: 480,
-    width: "100%",
+    width: '100%',
     padding: 20,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     borderRadius: 12,
     borderWidth: 1,
-    borderColor: "#ededed",
+    borderColor: '#ededed',
   },
   header: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     paddingTop: 3,
     marginBottom: 15,
   },
   headerLeft: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   headerTitle: {
     marginLeft: 8,
     fontSize: 20,
-    fontWeight: "700",
-    color: "#1F1F1F",
+    fontWeight: '700',
+    color: '#1F1F1F',
   },
   content: {},
-});
-export default EditAvatarModal;
+})
+export default EditAvatarModal
