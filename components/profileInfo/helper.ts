d7e49830149082ead9babde9486767ceb1348a7f
weishunyu <2072738187@qq.com>
2023-05-11 14:53:21 +0800

    feat: 更改pin逻辑

--- components/profileInfo/helper.ts
+++ components/profileInfo/helper.ts
@@ -1,4 +1,5 @@
 import systemConfig from '../../constants/System'
+import DefaultAvatar from '../../assets/images/chat/defaultAvatar.svg'
 export const genAvatarUrl = (avatar: string) => {
   if (!avatar)
     return 'https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/5049cfd8aca04680b5d1acb9d8b32cc1~tplv-k3u1fbpfcp-watermark.image?)'
@@ -9,10 +10,4 @@ export const genBotUrl = (avatar: string) => {
   if (!avatar)
     return 'https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/5049cfd8aca04680b5d1acb9d8b32cc1~tplv-k3u1fbpfcp-watermark.image?)'
   return avatar?.startsWith('bot-logo/') ? `${systemConfig?.avatarImgHost}${avatar}` : avatar
-}
-
-export const genBotLogo = (avatar: string) => {
-  if (!avatar)
-    return 'https://pics1.baidu.com/feed/55e736d12f2eb938661c830c4aa69e39e4dd6f28.jpeg@f_auto?token=b6ef42f11710ef742c703790f2a85d2b'
-  return avatar?.startsWith('bot/') ? `${systemConfig?.avatarImgHost}${avatar}` : avatar
 }
