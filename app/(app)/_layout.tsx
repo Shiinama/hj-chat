c2ecf9b379d854593145293737eee97bee56e09b
weishunyu <2072738187@qq.com>
2023-05-13 17:31:50 +0800

    fix: 解决安卓录音问题

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,31 +1,30 @@
-import { Stack, useNavigation } from "expo-router";
-import { Platform, TouchableOpacity, StyleSheet } from "react-native";
-import Back from "../../assets/images/tabbar/back.svg";
+import { Stack, useNavigation } from 'expo-router'
+import { Platform, TouchableOpacity, StyleSheet } from 'react-native'
+import Back from '../../assets/images/tabbar/back.svg'
 
 export default function AppLayout() {
-  const navigation = useNavigation();
+  const navigation = useNavigation()
   return (
     <Stack
       screenOptions={{
+        headerBackVisible: false,
+        headerTitleAlign: 'center',
         headerLeft: () => {
           return (
-            <TouchableOpacity
-              onPress={() => navigation.goBack()}
-              style={styles.back}
-            >
+            <TouchableOpacity onPress={() => navigation.goBack()} style={styles.back}>
               <Back></Back>
             </TouchableOpacity>
-          );
+          )
         },
       }}
     ></Stack>
-  );
+  )
 }
 const styles = StyleSheet.create({
   back: {
     width: 24,
     height: 24,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-});
+})
