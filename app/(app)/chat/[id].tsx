166a0e25b8f0a13536ef4de49262b2c060270c4d
winljm001 <819648983@qq.com>
2023-05-13 15:25:52 +0800

    feat: chat loading

--- app/(app)/chat/[id].tsx
+++ app/(app)/chat/[id].tsx
@@ -1,119 +1,144 @@
-import { Text, View, TouchableOpacity, AppState, FlatList, Alert } from 'react-native'
-import { v4 as uuidv4 } from 'uuid'
-import { useSearchParams, useNavigation, useRouter } from 'expo-router'
-import { useEffect, useRef, useState } from 'react'
-import ChatItem from '../../../components/chat/chatItem'
-import Container from '../../../components/chat/container'
-import { chatHistory } from '../../../api'
-import { Audio } from 'expo-av'
-import { useSetState } from 'ahooks'
-import ShellLoading from '../../../components/loading'
-import { useSocketIo } from '../../../components/chat/socket'
-import * as FileSystem from 'expo-file-system'
-import { Buffer } from 'buffer'
-import { ChatContext, ChatPageState } from './chatContext'
-import { Button, Toast } from '@fruits-chain/react-native-xiaoshu'
-import { convert4amToMp3 } from '../../../utils/convert'
-import botStore from '../../../store/botStore'
-import FlashIcon from '../../../components/flashIcon'
-import useUserStore from '../../../store/userStore'
-import AudioPayManagerSingle, { AudioPayManager } from '../../../components/chat/audioPlayManager'
-import { NativeEventSubscription } from 'react-native'
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  AppState,
+  FlatList,
+  Alert,
+} from "react-native";
+import { v4 as uuidv4 } from "uuid";
+import { useSearchParams, useNavigation, useRouter } from "expo-router";
+import { useEffect, useId, useMemo, useRef, useState } from "react";
+import ChatItem from "../../../components/chat/chatItem";
+import Container from "../../../components/chat/container";
+import { chatHistory } from "../../../api";
+import { Audio } from "expo-av";
+import { useSetState } from "ahooks";
+import ShellLoading from "../../../components/loading";
+import { useSocketIo } from "../../../components/chat/socket";
+import * as FileSystem from "expo-file-system";
+import { Buffer } from "buffer";
+import { ChatContext, ChatPageState } from "./chatContext";
+import { Button, Toast } from "@fruits-chain/react-native-xiaoshu";
+import { convert4amToMp3 } from "../../../utils/convert";
+import botStore from "../../../store/botStore";
+import FlashIcon from "../../../components/flashIcon";
+import useUserStore from "../../../store/userStore";
+import AudioPayManagerSingle, {
+  AudioPayManager,
+} from "../../../components/chat/audioPlayManager";
+import { NativeEventSubscription } from "react-native";
 
 export type ChatItem = {
-  id: number
-  uid: string
-  userId?: number
-  userUid?: string
-  status?: string
-  type?: string
-  replyUid?: string | null
-  text?: string
-  translation?: string | null
-  voiceUrl?: string | null
-  botId?: number
-  content?: string
-  createdDate?: string
-  updatedDate?: string
-  botUid?: string
-}
+  id: number;
+  uid: string;
+  userId?: number;
+  userUid?: string;
+  status?: string;
+  type?: string;
+  replyUid?: string | null;
+  text?: string;
+  translation?: string | null;
+  voiceUrl?: string | null;
+  botId?: number;
+  content?: string;
+  createdDate?: string;
+  updatedDate?: string;
+  botUid?: string;
+};
 
 export default function Chat({}) {
-  const { pinned, logo, name, uid, userId, energyPerChat } = botStore.getState()
-  const { profile } = useUserStore()
+  const { pinned, logo, name, uid, userId, energyPerChat } =
+    botStore.getState();
+  const { profile } = useUserStore();
 
   const eventAppState = useRef<{
-    appState?: NativeEventSubscription
-    audioManager: AudioPayManager
-    prev?: string
+    appState?: NativeEventSubscription;
+    audioManager: AudioPayManager;
+    prev?: string;
   }>({
     audioManager: AudioPayManagerSingle(),
-  })
-
+  });
   /** 页面数据上下文 */
   const [chatPageValue, setChatPageValue] = useSetState<ChatPageState>({
-    pageStatus: 'normal',
+    pageStatus: "normal",
     selectedItems: [],
-  })
-  const navigation = useNavigation()
-  const [message, resMessage, sendMessage, translationMessage, updateMessage] = useSocketIo()
-  const [recording, setRecording] = useState(null)
-  const [text, setText] = useState('')
-  const [loading, setLoading] = useState<boolean>(true)
-  const [chatData, setChatData] = useState<ChatItem[]>([])
-  const [durationMillis, setDurationMillis] = useState(0)
-  const [voice, setVoice] = useState(null)
-  const [translationTextIndex, setTranslationTextIndex] = useState(null)
-  const flatList = useRef<FlatList>()
-  const [showLoadMoring, setShowLoadMoring] = useState(false)
+  });
+  const navigation = useNavigation();
+  const {
+    message,
+    resMessage,
+    sendMessage,
+    translationMessage,
+    updateMessage,
+    isPending,
+  } = useSocketIo();
+  // 给loading生成一个随机id，这里是用replyUid来判断是否是回复的信息，loading展示为回复数据，所以随便弄一个id当id
+  const randomId = useId();
+
+  const [recording, setRecording] = useState(null);
+  const [text, setText] = useState("");
+  const [loading, setLoading] = useState<boolean>(true);
+  const [chatData, setChatData] = useState<ChatItem[]>([]);
+  const [durationMillis, setDurationMillis] = useState(0);
+  const [voice, setVoice] = useState(null);
+  const [translationTextIndex, setTranslationTextIndex] = useState(null);
+  const flatList = useRef<FlatList>();
+  const [showLoadMoring, setShowLoadMoring] = useState(false);
   const chatDataInfo = useRef({
     isTouchList: false, // 用户是否touch了list，用户touch后如果触发list的onEndReached才去加载更多
     pageSize: 10, // 每页加载多少条数据
     hasMore: true,
-  })
+  });
   useEffect(() => {
     try {
-      new Audio.Recording()
+      new Audio.Recording();
 
       Audio.setAudioModeAsync({
         allowsRecordingIOS: true,
         playsInSilentModeIOS: true,
-      })
+      });
     } catch (err) {
-      Toast('Failed to start recording')
+      Toast("Failed to start recording");
     }
-    loadData()
+    loadData();
     return () => {
       // 单列模式里面的sound销毁
-      AudioPayManagerSingle().destory()
-    }
-  }, [])
+      AudioPayManagerSingle().destory();
+    };
+  }, []);
   async function startRecording() {
     try {
       await Audio.setAudioModeAsync({
         allowsRecordingIOS: true,
-      })
-      const defaultParam = Audio.RecordingOptionsPresets.HIGH_QUALITY
-      const { recording } = await Audio.Recording.createAsync(defaultParam, status => {
-        if (status.isRecording) {
-          setDurationMillis(status.durationMillis)
+      });
+      const defaultParam = Audio.RecordingOptionsPresets.HIGH_QUALITY;
+      const { recording } = await Audio.Recording.createAsync(
+        defaultParam,
+        (status) => {
+          if (status.isRecording) {
+            setDurationMillis(status.durationMillis);
+          }
         }
-      })
-      setRecording(recording)
+      );
+      setRecording(recording);
     } catch (err) {
-      Toast('Failed to start recording')
+      Toast("Failed to start recording");
     }
   }
 
   const loadData = (loadMore?: boolean) => {
     if (loadMore) {
-      setShowLoadMoring(true)
+      setShowLoadMoring(true);
     }
     chatHistory({
       botUid: uid,
       offset: loadMore ? chatData.length : 0,
       limit: chatDataInfo.current.pageSize,
-      beforeId: chatData.length > 0 && loadMore ? chatData[chatData.length - 1].id : undefined,
+      beforeId:
+        chatData.length > 0 && loadMore
+          ? chatData[chatData.length - 1].id
+          : undefined,
     })
       .then(({ data }: any) => {
         // fix 手动颠倒顺序滚动位置无法精准的问题以及其他滚动问题 FlatList设置了inverted(倒置，往上滑就是加载更多了 上变为下，数据也是一样)就无需排序和调用scrollEnd了
@@ -123,134 +148,153 @@ export default function Chat({}) {
         // );
         // chatDataInfo.current.data = data
         // console.log(data, chatData)
-        setChatData(!loadMore ? data : [...chatData, ...data])
+        setChatData(!loadMore ? data : [...chatData, ...data]);
         if (data.length < chatDataInfo.current.pageSize) {
-          chatDataInfo.current.hasMore = false
+          chatDataInfo.current.hasMore = false;
         }
-        chatDataInfo.current.isTouchList = false
-        setShowLoadMoring(false)
-        setLoading(false)
+        chatDataInfo.current.isTouchList = false;
+        setShowLoadMoring(false);
+        setLoading(false);
       })
-      .catch(e => {
-        console.log(e)
-        chatDataInfo.current.isTouchList = false
-        setShowLoadMoring(false)
-        setLoading(false)
-      })
-  }
+      .catch((e) => {
+        console.log(e);
+        chatDataInfo.current.isTouchList = false;
+        setShowLoadMoring(false);
+        setLoading(false);
+      });
+  };
 
   async function stopRecording() {
     try {
-      await recording.stopAndUnloadAsync()
-      const uri = recording.getURI()
-      const mp3Uri = await convert4amToMp3(uri)
+      await recording.stopAndUnloadAsync();
+      const uri = recording.getURI();
+      const mp3Uri = await convert4amToMp3(uri);
       const buffer = await FileSystem.readAsStringAsync(mp3Uri, {
         encoding: FileSystem.EncodingType.Base64,
-      })
-      const fileBuffer = Buffer.from(`data:audio/mpeg;base64,${buffer}`, 'base64')
-      setVoice(fileBuffer)
-      return uri
+      });
+      const fileBuffer = Buffer.from(
+        `data:audio/mpeg;base64,${buffer}`,
+        "base64"
+      );
+      setVoice(fileBuffer);
+      return uri;
     } catch (err) {
-      console.error('Failed to stop recording', err)
+      console.error("Failed to stop recording", err);
     }
   }
   function setAuInfo() {
-    sendAudio()
+    sendAudio();
   }
 
   const sendAudio = () => {
-    const reqId = uuidv4()
-    sendMessage('voice_chat', {
+    const reqId = uuidv4();
+    sendMessage("voice_chat", {
       reqId,
       botUid: uid,
       voice,
-    })
-  }
-  const translationText = messageUid => {
-    const Index = chatData.findIndex(item => item.uid === messageUid)
-    if (chatData[Index].translation) return
-    setTranslationTextIndex(Index)
-    const reqId = uuidv4()
-    sendMessage('translate_message', {
+    });
+  };
+  const translationText = (messageUid) => {
+    const Index = chatData.findIndex((item) => item.uid === messageUid);
+    if (chatData[Index].translation) return;
+    setTranslationTextIndex(Index);
+    const reqId = uuidv4();
+    sendMessage("translate_message", {
       reqId,
       messageUid,
-    })
-  }
+    });
+  };
   useEffect(() => {
     navigation.setOptions({
       headerTitle: () => (
-        <View style={{ flexDirection: 'row' }}>
-          <Text style={{ fontSize: 18, fontWeight: '600' }}>{name}</Text>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={{ fontSize: 18, fontWeight: "600" }}>{name}</Text>
           <FlashIcon energyPerChat={energyPerChat} />
         </View>
       ),
       headerRight: () => {
         return (
-          chatPageValue.pageStatus === 'sharing' && (
+          chatPageValue.pageStatus === "sharing" && (
             <Button
               type="ghost"
               text="Cancel"
               color="#7A2EF6"
               size="s"
               style={{ borderRadius: 8 }}
-              onPress={() => setChatPageValue({ pageStatus: 'normal' })}
+              onPress={() => setChatPageValue({ pageStatus: "normal" })}
             />
           )
-        )
+        );
       },
-    })
-  }, [navigation, name, chatPageValue.pageStatus])
+    });
+  }, [navigation, name, chatPageValue.pageStatus]);
 
   useEffect(() => {
-    if (!message) return
-    setChatData([message.data, ...chatData])
-    flatList.current?.scrollToIndex?.({ index: 0 })
-  }, [message])
+    if (!message) return;
+    setChatData([message.data, ...chatData]);
+    flatList.current?.scrollToIndex?.({ index: 0 });
+  }, [message]);
 
   useEffect(() => {
-    if (!resMessage) return
-    setChatData([resMessage, ...chatData])
-    if (resMessage?.voiceUrl?.length > 0 && !AudioPayManagerSingle().currentAutoPlayUrl) {
-      AudioPayManagerSingle().currentAutoPlayUrl = resMessage?.voiceUrl
+    if (!resMessage) return;
+    setChatData([resMessage, ...chatData]);
+    if (
+      resMessage?.voiceUrl?.length > 0 &&
+      !AudioPayManagerSingle().currentAutoPlayUrl
+    ) {
+      AudioPayManagerSingle().currentAutoPlayUrl = resMessage?.voiceUrl;
     }
-    flatList.current?.scrollToIndex?.({ index: 0 })
-  }, [resMessage])
+    flatList.current?.scrollToIndex?.({ index: 0 });
+  }, [resMessage]);
 
   useEffect(() => {
-    if (!updateMessage) return
-    const index = chatData.findIndex(item => item.uid === updateMessage.uid)
-    setChatData(pre => {
-      pre[index].text = updateMessage.text
-      return [...pre]
-    })
-  }, [updateMessage])
+    if (!updateMessage) return;
+    const index = chatData.findIndex((item) => item.uid === updateMessage.uid);
+    setChatData((pre) => {
+      pre[index].text = updateMessage.text;
+      return [...pre];
+    });
+  }, [updateMessage]);
 
   useEffect(() => {
-    if (!translationMessage) return
-    setChatData(pre => {
+    if (!translationMessage) return;
+    setChatData((pre) => {
       // fix TypeError: undefined is not an object (evaluating 'pre[translationTextIndex].translation = translationMessage.translation')
       if (pre[translationTextIndex]) {
-        pre[translationTextIndex].translation = translationMessage.translation
+        pre[translationTextIndex].translation = translationMessage.translation;
       }
 
-      return [...pre]
-    })
-  }, [translationMessage])
+      return [...pre];
+    });
+  }, [translationMessage]);
 
   const loadNextData = () => {
     try {
-      if (!chatDataInfo.current.hasMore || !chatDataInfo.current.isTouchList || showLoadMoring) {
-        return
+      if (
+        !chatDataInfo.current.hasMore ||
+        !chatDataInfo.current.isTouchList ||
+        showLoadMoring
+      ) {
+        return;
       }
-      loadData(true)
+      loadData(true);
     } catch (e) {
-      console.log(e || 'loadNextData error')
+      console.log(e || "loadNextData error");
     }
-  }
+  };
 
-  if (loading) return <ShellLoading></ShellLoading>
+  const listData = useMemo(() => {
+    if (isPending && chatData?.length > 0) {
+      return [{ type: "LOADING", replyUid: randomId }, ...chatData];
+    } else {
+      return chatData;
+    }
+  }, [chatData, isPending, randomId]);
+  if (loading) return <ShellLoading></ShellLoading>;
   return (
-    <ChatContext.Provider value={{ value: chatPageValue, setValue: setChatPageValue }}>
+    <ChatContext.Provider
+      value={{ value: chatPageValue, setValue: setChatPageValue }}
+    >
       <Container
         inputTextProps={
           {
@@ -265,40 +309,41 @@ export default function Chat({}) {
             stopRecording,
             onSubmitEditing: async (value: any) => {
               if (value.length === 0) {
-                Alert.alert('Please enter your message')
-                return
+                Alert.alert("Please enter your message");
+                return;
               }
-              const reqId = uuidv4()
-              sendMessage('text_chat', {
+              const reqId = uuidv4();
+
+              sendMessage("text_chat", {
                 reqId,
                 botUid: uid,
                 text: value,
-              })
-              setText('')
+              });
+              setText("");
             },
           } as any
         }
         flatListRef={flatList}
         flatListProps={{
-          data: chatData,
+          data: listData,
           inverted: true,
           onTouchStart: () => {
             // inverted: true 颠倒列表，往上滑就是加载更多了 上变为下，数据也是一样，加载完数据就无需排序和调用scrollEnd了，并且新增一条消息也无需调用scrollEnd
             // 防止进来渲染数据的时候 触发onEndReached去加载更多，用户手动滑动的时候再去加载更多
-            chatDataInfo.current.isTouchList = true
+            chatDataInfo.current.isTouchList = true;
           },
           onEndReached: () => {
             // 分页，颠倒列表 inverted为true往上滑就会调用此方法，原来是往下滑调用这个方法，往上滑是调用onRefresh
-            loadNextData()
+            loadNextData();
           },
           ListFooterComponent: showLoadMoring ? (
             <View
               style={{
-                width: '100%',
+                width: "100%",
                 marginVertical: 15,
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
               }}
             >
               <ShellLoading />
@@ -307,13 +352,18 @@ export default function Chat({}) {
           onEndReachedThreshold: 0.2,
           renderItem: ({ item, index }) => {
             return (
-              <View>
-                <ChatItem me={profile?.avatar} logo={logo} translationText={translationText} item={item}></ChatItem>
+              <View key={index}>
+                <ChatItem
+                  me={profile?.avatar}
+                  logo={logo}
+                  translationText={translationText}
+                  item={item}
+                ></ChatItem>
               </View>
-            )
+            );
           },
         }}
       ></Container>
     </ChatContext.Provider>
-  )
+  );
 }
--- components/chat/styles.ts
+++ components/chat/styles.ts
@@ -1,56 +1,55 @@
-import { StyleSheet } from 'react-native'
-import Colors from '../../constants/Colors'
+import { StyleSheet } from "react-native";
+import Colors from "../../constants/Colors";
 export default StyleSheet.create({
   avatar: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     width: 32,
     height: 32,
     borderRadius: 16,
   },
 
   itemWrap: {
-    flexDirection: 'row',
-    alignItems: 'flex-start',
+    flexDirection: "row",
+    alignItems: "flex-start",
   },
   msgBox: {
     flexShrink: 1,
     flexGrow: 1,
     marginBottom: 24,
-    alignItems: 'flex-start',
+    alignItems: "flex-start",
   },
   you: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   me: {
-    flexDirection: 'row-reverse',
+    flexDirection: "row-reverse",
   },
   contentBox: {
     borderRadius: 5,
     marginLeft: 8,
     marginRight: 8,
     maxWidth: 263,
-    
   },
   button: {
     borderRadius: 12,
-    flexDirection: 'row',
+    flexDirection: "row",
     margin: 12,
     paddingHorizontal: 6,
     paddingVertical: 4,
-    alignItems: 'center',
+    alignItems: "center",
     backgroundColor: Colors.mainWhite,
   },
   active: {
-    backgroundColor: '#7A2EF6',
+    backgroundColor: "#7A2EF6",
     color: Colors.mainWhite,
   },
   content: {
     padding: 12,
-    width: '100%',
+    width: "100%",
     borderTopWidth: 1,
     fontSize: 14,
-    borderColor: '#E2E8F0',
+    borderColor: "#E2E8F0",
     borderBottomWidth: 1,
   },
   textContent: {
@@ -58,17 +57,17 @@ export default StyleSheet.create({
     borderBottomWidth: 0,
   },
   buttonGroup: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   youContent: {
     backgroundColor: Colors.mainGrey,
   },
   meContent: {
-    backgroundColor: '#F1EAFE',
+    backgroundColor: "#F1EAFE",
   },
   footer: {
-    backgroundColor: '#f5f5f5',
-    display: 'flex',
+    backgroundColor: "#f5f5f5",
+    display: "flex",
     paddingVertical: 6,
     paddingHorizontal: 12,
   },
@@ -76,13 +75,13 @@ export default StyleSheet.create({
     height: 30,
     paddingLeft: 8,
     paddingRight: 8,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 5,
   },
   sendBtn: {
     width: 60,
     height: 30,
-    backgroundColor: '#9F9F5F',
+    backgroundColor: "#9F9F5F",
   },
   picture: {
     borderRadius: 5,
@@ -93,11 +92,25 @@ export default StyleSheet.create({
     marginLeft: 11,
   },
   absolute: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
     bottom: 0,
     right: 0,
     zIndex: 999,
   },
-})
+  loadingBox: {
+    height: 42,
+    backgroundColor: Colors.mainGrey,
+    paddingHorizontal: 16,
+    borderRadius: 12,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  loadingText: {
+    fontSize: 14,
+  },
+  loadingIcon: {
+    marginLeft: 8,
+  },
+});
