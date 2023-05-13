dca25b4f56223b8157ef8b40699e1e92a007eb1c
winljm001 <819648983@qq.com>
2023-05-13 19:04:45 +0800

    fix: ProgressBar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,27 +1,27 @@
-import { Tabs, useFocusEffect } from 'expo-router'
-import { View, Image, Text } from 'react-native'
-import ProgressBar from '../../components/ProgressBar'
-import Flash from '../../assets/images/tabbar/flash.svg'
-import Chat from '../../assets/images/tabbar/chat.svg'
-import ChatAcitve from '../../assets/images/tabbar/chat_acitve.svg'
-import Profile from '../../assets/images/tabbar/profile.svg'
-import ProfileAcitve from '../../assets/images/tabbar/profile_acitve.svg'
-import Bot from '../../assets/images/tabbar/bot.svg'
-import BotAcitve from '../../assets/images/tabbar/bot_active.svg'
-import myshell from '../../assets/images/myshell.png'
-import { useCallback, useEffect, useState } from 'react'
-import useUserStore, { getUserEnergyInfo } from '../../store/userStore'
+import { Tabs, useFocusEffect } from "expo-router";
+import { View, Image, Text } from "react-native";
+import ProgressBar from "../../components/ProgressBar";
+import flashImg from "../../assets/images/tabbar/flash.png";
+import Chat from "../../assets/images/tabbar/chat.svg";
+import ChatAcitve from "../../assets/images/tabbar/chat_acitve.svg";
+import Profile from "../../assets/images/tabbar/profile.svg";
+import ProfileAcitve from "../../assets/images/tabbar/profile_acitve.svg";
+import Bot from "../../assets/images/tabbar/bot.svg";
+import BotAcitve from "../../assets/images/tabbar/bot_active.svg";
+import myshell from "../../assets/images/myshell.png";
+import { useCallback, useEffect, useState } from "react";
+import useUserStore, { getUserEnergyInfo } from "../../store/userStore";
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
 
 export default function TabLayout() {
-  const { userEnergyInfo: energy } = useUserStore()
+  const { userEnergyInfo: energy } = useUserStore();
   useFocusEffect(
     useCallback(() => {
-      getUserEnergyInfo()
+      getUserEnergyInfo();
     }, [])
-  )
+  );
   return (
     <Tabs
       screenOptions={{
@@ -32,50 +32,52 @@ export default function TabLayout() {
         name="index"
         options={{
           tabBarShowLabel: false,
-          title: '',
+          title: "",
           tabBarIcon: ({ focused }) => (focused ? <ChatAcitve /> : <Chat />),
           headerLeft: () => (
             <View
               style={{
                 marginLeft: 16,
-                flexDirection: 'row',
-                alignItems: 'center',
+                flexDirection: "row",
+                alignItems: "center",
               }}
             >
               <Image style={{ width: 20, height: 20 }} source={myshell}></Image>
-              <Text style={{ fontSize: 18, lineHeight: 28, marginLeft: 4 }}>MySheel</Text>
+              <Text style={{ fontSize: 18, lineHeight: 28, marginLeft: 4 }}>
+                MySheel
+              </Text>
             </View>
           ),
           headerRight: () => (
             <View
               style={{
                 marginRight: 16,
-                flexDirection: 'row',
-                alignItems: 'center',
+                flexDirection: "row",
+                alignItems: "center",
+                width: 75,
+                position: "relative",
+                justifyContent: "flex-end",
               }}
             >
-              <View
-                style={{
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  width: 28,
-                  height: 28,
-                  borderTopWidth: 1,
-                  borderBottomWidth: 1,
-                  borderLeftWidth: 1,
-                  borderColor: '#F6F6F6',
-                  borderRadius: 8,
-                }}
-              >
-                <Flash></Flash>
+              <View style={{ width: 60 }}>
+                {energy && (
+                  <ProgressBar
+                    size="s"
+                    progressValue={energy.energy}
+                    maxRange={energy.dailyEnergy}
+                  ></ProgressBar>
+                )}
               </View>
-              {energy && (
-                <ProgressBar
-                  progressBarColor="#FFC03A"
-                  progressValue={energy.energy}
-                  maxRange={energy.dailyEnergy}
-                ></ProgressBar>
-              )}
+              <Image
+                source={flashImg}
+                style={{
+                  width: 24,
+                  height: 24,
+                  position: "absolute",
+                  left: 0,
+                  zIndex: 10,
+                }}
+              />
             </View>
           ),
         }}
@@ -84,7 +86,7 @@ export default function TabLayout() {
         name="setting"
         options={{
           tabBarShowLabel: false,
-          title: 'Robot Workshop',
+          title: "Robot Workshop",
           tabBarIcon: ({ focused }) => (focused ? <BotAcitve /> : <Bot />),
         }}
       />
@@ -92,10 +94,11 @@ export default function TabLayout() {
         name="profile"
         options={{
           tabBarShowLabel: false,
-          title: 'Profile',
-          tabBarIcon: ({ focused }) => (focused ? <ProfileAcitve /> : <Profile />),
+          title: "Profile",
+          tabBarIcon: ({ focused }) =>
+            focused ? <ProfileAcitve /> : <Profile />,
         }}
       />
     </Tabs>
-  )
+  );
 }
