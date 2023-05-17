c13b3f403e501b9025186d2b2102439de378d9de
weishunyu <2072738187@qq.com>
2023-05-17 21:38:34 +0800

    feat: build

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import 'react-native-get-random-values'
 import './tmp/pollify'
 
 //test particle
-// import { ParticleInfo } from './tmp/NetService/ParticleInfo'
+import { ParticleInfo } from './tmp/NetService/ParticleInfo'
 
 import * as particleAuth from 'react-native-particle-auth'
 import { ChainInfo, Env } from 'react-native-particle-auth'
@@ -18,14 +18,14 @@ const init = async () => {
   // Get your project id and client from dashboard,
   // https://dashboard.particle.network/
   // 生产
-  // ParticleInfo.projectId = 'cb5e91db-f37b-4e53-8107-b97e36f78072' // your project id
-  // ParticleInfo.clientKey = 'cPllcC2s2NmUZKJLmdzKyJZDIzz70Vasg5kwTsrx' // your client key
+  ParticleInfo.projectId = 'cb5e91db-f37b-4e53-8107-b97e36f78072' // your project id
+  ParticleInfo.clientKey = 'cPllcC2s2NmUZKJLmdzKyJZDIzz70Vasg5kwTsrx' // your client key
   // ParticleInfo.projectId = 'c9aa126d-8db2-45cc-8898-60e3a69d5050' // your project id
   // ParticleInfo.clientKey = 'cOyQSJfazQ5zu32GwvW7AvBz0f7q0RWUIULWMZhk' // your client key
 
-  // if (ParticleInfo.projectId == '' || ParticleInfo.clientKey == '') {
-  //   throw new Error('You need set project info')
-  // }
+  if (ParticleInfo.projectId == '' || ParticleInfo.clientKey == '') {
+    throw new Error('You need set project info')
+  }
 
   const chainInfo = ChainInfo.EthereumGoerli
   // const chainInfo = EvmService.currentChainInfo;
