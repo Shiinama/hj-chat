ad530d35d0adc009c2bf310bd0c188fa943f88b2
winljm001 <819648983@qq.com>
2023-05-20 14:50:35 +0800

    feat: list 搜索完成

--- components/setting/AllRobot/index.tsx
+++ components/setting/AllRobot/index.tsx
@@ -1,15 +1,22 @@
 import { FC, useState } from 'react'
 import { View, StyleSheet } from 'react-native'
 import CreateCard from '../CreateCard'
+import RobotList from '../RobotList'
 import SearchInput from '../SearchInput'
+import { useDebounceEffect } from 'ahooks'
 
 export interface AllRobotProps {}
 const AllRobot: FC<AllRobotProps> = () => {
-  const [keyword, setKeyword] = useState('')
+  const [name, setName] = useState('')
+
+  // /bot/getUgcBotList?tagId=1&name=xxx
+  // /bot/getUgcBotList
+  // /bot/ugc/own
   return (
     <View style={styles.page}>
-      <SearchInput value={keyword} onChange={setKeyword} />
+      <SearchInput value={name} onChange={setName} />
       <CreateCard />
+      <RobotList requestParams={{ name }} />
     </View>
   )
 }
---
+++ components/setting/RobotList/index.tsx
@@ -0,0 +1,88 @@
+import botStore from '../../../store/botStore'
+import { useBoolean, useDebounceEffect } from 'ahooks'
+import { useRouter } from 'expo-router'
+import { FC, useEffect, useState } from 'react'
+import { View, Text } from 'react-native'
+import { getUgcBotList } from '../../../api/robot'
+import BotCard from '../../botCard'
+import ShellLoading from '../../loading'
+import CallBackManagerSingle from '../../../utils/CallBackManager'
+
+export interface RobotListProps {
+  /** 请求的参数 */
+  requestParams: any
+}
+const RobotList: FC<RobotListProps> = ({ requestParams }) => {
+  const [robotListData, setRobotListData] = useState([])
+  const [loading, { setFalse, setTrue }] = useBoolean(false)
+  useEffect(() => {
+    CallBackManagerSingle().add('ugcbotList', botUid => {
+      loadData(botUid)
+    })
+    loadData()
+    return () => {
+      CallBackManagerSingle().remove('ugcbotList')
+    }
+  }, [])
+  const loadData = (botUid?: string) => {
+    setTrue()
+    getUgcBotList(requestParams)
+      .then((res: any) => {
+        if (botUid) {
+          botStore.setState(res.find(item => item.uid === botUid))
+        }
+        setRobotListData(res)
+      })
+      .finally(() => {
+        setFalse()
+      })
+  }
+  useDebounceEffect(
+    () => {
+      loadData()
+    },
+    [requestParams],
+    {
+      wait: 400,
+    }
+  )
+  const router = useRouter()
+  const onShowDetail = event => {
+    botStore.setState(event)
+    router.push({
+      pathname: `robot/${event.id}`,
+      params: {
+        id: event.id,
+        tag: event.tag,
+        userId: event.userId,
+        status: event.status,
+        name: event.name,
+        language: event.language,
+        uid: event.uid,
+      },
+    })
+  }
+  if (loading)
+    return (
+      <View style={{ minHeight: 210, alignItems: 'center' }}>
+        <ShellLoading></ShellLoading>
+      </View>
+    )
+  return (
+    <View>
+      {robotListData?.map((ld, i) => {
+        return (
+          <BotCard
+            onShowDetail={e => {
+              onShowDetail(e)
+            }}
+            key={ld.id}
+            ld={ld}
+            showTime={false}
+          />
+        )
+      })}
+    </View>
+  )
+}
+export default RobotList
